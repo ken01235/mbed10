8f563359fee9330e1395663a9cec87931654e537
ken01235 <kenkenrc08@gmail.com>
2021-05-04 05:00:12 -0700

    all

---
+++ BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/RTX/Library/cmsis_os1.d
@@ -0,0 +1,6 @@
+BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/RTX/Library/cmsis_os1.o: \
+ mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/RTX/Library/cmsis_os1.c \
+ BUILD/B_L4S5I_IOT01A/GCC_ARM/mbed_config.h \
+ mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/RTX/Include1/cmsis_os.h \
+ mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/Include/cmsis_os2.h \
+ mbed-os-example-sockets/mbed-os/cmsis/CMSIS_5/CMSIS/RTOS2/RTX/Include/rtx_os.h
